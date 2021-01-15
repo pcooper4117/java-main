658945f0914896d64dc3d388792d1748d31e37ae
Patrick Cooper <pcooper4117@gmail.com>
2021-01-14 23:51:26 -0500

    day-four-holy-crap-dude

--- module-1/04_Loops_Arrays/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.lang.reflect.Array;
+
 public class Exercises {
 	
 	/*
@@ -10,6 +12,10 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
+		if (nums[0] == 6 || nums[nums.length-1] == 6) {
+			return true;
+		}
+		
 		return false;
 	}
 
@@ -21,6 +27,17 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
+		// check if array passed has at least one element. if not false.
+		// compare first and last elements to see if theyre equal, if they are return true
+		//															if not return false
+		if (nums.length < 1) { 
+			return false;
+		}
+		if ( nums[0] == nums[nums.length-1]) {
+			return true;
+		}
+		
+		
 		return false;
 	}
 
@@ -29,9 +46,10 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		int [] result = new int[] {3,1,4};
+		return result;
 	}
-
+	
 	/*
 	 4. Given 2 arrays of ints, a and b, return true if they have the same first element or they have 
 	 the same last element. Both arrays will be length 1 or more.
@@ -39,17 +57,27 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [7, 3, 2]) → false
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
+	// return true if same first element
+	//return true if same last element
+	//
 	public boolean commonEnd(int[] a, int[] b) {
-		return false;
-	}
-
+	
+		return (a[0] == b[0] || a[a.length-1] == b[b.length-1]);} 
+		
+	
+	
+		
 	/*
 	 5. Given an array of ints length 3, return the sum of all the elements.
 	 sum3([1, 2, 3]) → 6
 	 sum3([5, 11, 2]) → 18
 	 sum3([7, 0, 0]) → 7
 	 */
+//	int sum = 0;
 	public int sum3(int[] nums) {
+		for (int i=0; i < nums.length; i++) {
+	//		sum = sum + nums[i];
+		}
 		return 0;
 	}
 
@@ -60,8 +88,10 @@ public class Exercises {
 	 rotateLeft3([5, 11, 9]) → [11, 9, 5]
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
+	//create a
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+		int[] rotated = {nums[1], nums[2], nums[0]};
+		return rotated;
 	}
 
 	/*
@@ -71,8 +101,10 @@ public class Exercises {
 	 reverse3([5, 11, 9]) → [9, 11, 5]
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
-	public int[] reverse3(int[] nums) {
-		return new int[] {};
+	public int[] reverse3(int[] nums)
+	{
+		int [] reversed = {nums[2],nums[1], nums [0]};
+		return reversed;
 	}
 
 	/*
@@ -83,7 +115,19 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+		int maxValue = 0;
+		if (nums [0] <= nums [2]) {
+		 maxValue = nums[2];
+		}
+		
+		if (nums [2] <= nums [0]) {
+			 maxValue = nums[0];
+		}
+		for (int i = 0; i < nums.length; i ++ ){
+			nums[i] = maxValue;
+		}
+		
+		return nums;
 	}
 
 	/*
@@ -94,7 +138,17 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+		if (nums.length == 0) {
+			return 0;
+		}
+		if (nums.length < 2) {
+			return nums[0];
+		}
+	
+		return 	nums[0] + nums [1];
+	
+		
+		
 	}
 
 	/*
@@ -104,10 +158,18 @@ public class Exercises {
 	 middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
+	
+//  
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
-	}
-
+		
+		//int mid1 = a[1];
+		//int mid2 = b[1];
+		// int [] newArray = [mid1 + mid2];
+		// return newArray; 
+	
+	int[] middleArray = {a[1], b[1]};
+	return middleArray;}
+		
 	/*
 	 11. Return the number of even ints in the given array. Note: the % "mod" operator computes the 
 	 remainder, e.g. 5 % 2 is 1.
@@ -116,7 +178,18 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int numEven = 0;
+		int i;
+		for ( i = 0; i < nums.length; i++);
+		{ 
+			if (( nums[i] %2 ) == 0);
+					{
+				numEven ++;
+					}
+		}
+		
+		
+		return numEven;
 	}
 
 	/*
@@ -129,17 +202,28 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13, 3]) → 6
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
+	// return sum of array if 0;
+	//
 	public int sum13(int[] nums) {
-		return 0;
+		 int sum = 0;
+		for (int i = 0; i < nums.length ; i++) {
+	
+	sum = sum + nums[i];
 	}
-
+		if (sum == 0);{
+			return 0;
+		}
+	
 	/*
 	 13. Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
 	 has22([1, 2, 2]) → true
 	 has22([1, 2, 1, 2]) → false
 	 has22([2, 1, 2]) → false
 	 */
+	//if statement for array has two 2s;
 	public boolean has22(int[] nums) {
+		
+		
 		return false;
 	}
 	
@@ -149,9 +233,19 @@ public class Exercises {
 	 lucky13([1, 2, 3]) → false
 	 lucky13([1, 2, 4]) → false
 	 */
-	public boolean lucky13(int[] nums) {
-		return false;
+	//public boolean lucky13(int[] nums) {
+//		if (nums [0] == 1 && nums[0] == 3);{
+	//	return false;
+	//}
+	//	if (nums [1] == 1 && nums[1] == 3);
+	//return false;
+	//}
+	//	if (nums [2] == 1 && nums[2] == 3);
+//return false;
+	//}	else {
+//	return true;
 	}
+
 
 	/*
 	 15. Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
@@ -164,3 +258,4 @@ public class Exercises {
 	}
 
 }
+
--- module-1/04_Loops_Arrays/student-lecture-Frank/java/src/main/java/com/techelevator/FranksArrayExample.java
+++ module-1/04_Loops_Arrays/student-lecture-Frank/java/src/main/java/com/techelevator/FranksArrayExample.java
@@ -5,7 +5,51 @@ public class FranksArrayExample {
 	public static void main(String[] args) {  
 		
 		System.out.println("Welcome to Frank's Array Example Program"); 
+	/	int score1 = 10;
+			int score2 = 20;
+			int score3 = 30;
+				int score4 = 50;
+			int score5 = 90;
+			
+			int sum = 0; // this will hold sum of scores
+			double avg = 0.0; // this will hold averege of scores
+					
+					
+				sum = score1 + score2 + score3 + score4 + score5; // a change in data require a change in code
+		avg = sum / 5 ;
+			
+			
+			System.out.println ("Sum of scores: " + sum);
+			System.out.println ("averege of scores: " + avg);
 
+		
+		//
+			// Use an array to make the code loosely coupled
+			// loosley coupled code means code is not directly bound to data
+			// changes to data does not require change to code - sign of professional programmer
+			// 		for (int i=0; i <scores.length; i++)
+			//		i will be set to 0
+			//		check to see if i is less than the number of elements in scores array (i<3) - i=0,1,2 we loop; i = 3 we exit loop
+			//		execute statement in block
+			//		increment i
+			//		go back to step 2
+			//		
+			
+			int[] scores = new int[5];
+			scores [0] = 10;
+			scores [1] = 20;
+			scores[2] = 30;
+			scores[3]  = 80;
+			scores [4] = 120;
+			sum = 0;
+			for(int i = 0;  i < scores.length; i++) {
+				sum = sum + scores[i]; // add element in scores[i] to sum; scores[i] is current element;
+			
 	}   
-   
+   //			at the end of loop sum will contain total of all elements
+			avg = sum / scores.length; 
+			
+			System.out.println ("Sum of scores: " + sum);
+			System.out.println ("averege of scores: " + avg);
+	}
 }
