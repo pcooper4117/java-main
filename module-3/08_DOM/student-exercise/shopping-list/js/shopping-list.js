c1e5e8be83ce27718d8f4466f0626e246a378c2d
Patrick Cooper <pcooper4117@gmail.com>
2021-03-18 08:59:23 -0400

    update

--- module-3/08_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,7 +1,7 @@
 // add pageTitle
 const pageTitle = 'My Shopping List';
 // add groceries
-const groceries = ['Meat', 'Beer', 'Veggies', 'Eggs', 'Bread', 'Milk', 'Milk', 'T.P.', 'Fruit', 'Gatorade'];
+const groceries = ['Meat', 'Beer', 'Veggies', 'Eggs', 'Bread', 'Water', 'Milk', 'T.P.', 'Fruit', 'Gatorade'];
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
@@ -38,7 +38,7 @@ displayGroceries();
 // Don't worry too much about what is going on here, we will cover this when we discuss events.
 document.addEventListener('DOMContentLoaded', () => {
   // When the DOM Content has loaded attach a click listener to the button
-  
+
   const button = document.querySelector('.btn');
   button.addEventListener('click', markCompleted);
 });
